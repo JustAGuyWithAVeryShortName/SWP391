8fc67457a6589899381078321d6412b18b4cfad1
xuzzz1 <nguyentandat97pch@gmail.com>
2025-05-28 20:10:57 +0700

    swp1

---
+++ swp/src/main/java/com/xuz/app/controller/HomeController.java
@@ -0,0 +1,9 @@
+package com.xuz.app.controller;
+
+public class HomeController {
+
+    public String home() {
+        return "index";
+        // Spring Boot sẽ tự tìm file ở: src/main/resources/templates/index.html
+        }
+        }
---
+++ swp/src/main/java/com/xuz/app/controller/Main.java
@@ -0,0 +1,5 @@
+package com.xuz.app.controller;
+
+public class Main {
+    public static void main(String[] args) {}
+}
