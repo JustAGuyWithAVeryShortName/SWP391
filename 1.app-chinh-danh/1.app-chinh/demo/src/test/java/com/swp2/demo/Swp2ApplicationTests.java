6fcdf17cf2b41f11f83986889ddeb46e0cba4443
DanhCao2703 <danhcao270305@gmail.com>
2025-06-07 16:26:45 +0700

    danh

---
+++ 1.app-chinh-danh/1.app-chinh/demo/src/test/java/com/swp2/demo/Swp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
